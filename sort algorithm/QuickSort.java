d9ce3610107e23dbf6d46948265ebb6bab9b5ccc
chengwei <yy316004@gmail.com>
2019-10-09 22:05:47 +0800

    add quick sort class

---
+++ sort algorithm/QuickSort.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class QuickSort {
+
+    public static void main(String []args) {
+       int [] Array = {9,6,5,3,1,2,4,7,8};
+       int [] SortedArray = QuickSortFunc(Array);
+       System.out.println(Arrays.toString(Array));
+    }
+
+    public static int [] QuickSortFunc(int [] A)
+    {
+        int [] Result;
+        return A;
+    }
+
+    public static void Swap(int a)
+    {
+
+    }
+ }
