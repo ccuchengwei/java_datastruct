c3946f5fa793ba756cb46fbc5a78cf113f521ab9
chengwei <yy316004@gmail.com>
2019-10-12 11:38:56 +0800

    Implement Test.Class to test Linklist.Class

--- List/TestList.java
+++ List/TestList.java
@@ -4,5 +4,16 @@ public class TestList
     public static void main(String [] args)
     {
         LinkList<Integer> testList1 = new LinkList<Integer>();
+        testList1.append(1);
+        testList1.append(2);
+        testList1.append(3);
+        
+        LinkList.Node currentNode = testList1.GetHead();
+
+        do {
+            System.out.println(currentNode.value);
+            currentNode = currentNode.next;
+        } while (currentNode.value != null);
+        
     }
 }
--- List/linklist/LinkList.java
+++ List/linklist/LinkList.java
@@ -2,13 +2,12 @@ package linklist;
 
 public class LinkList<T>
 {
-
-    private class Node<T>
+    public class Node<T>
     {
-        Node<T> next;
-        T value;
+        public Node<T> next;
+        public T value;
 
-        private Node(T value, Node<T> next) {
+        public Node(T value, Node<T> next) {
             this.value = value;
             this.next = next;
         }
@@ -16,14 +15,20 @@ public class LinkList<T>
 
     private int size;
     private Node<T> head;
-
+    private Node<T> currentNode;
     public int GetSize()
     {
         return this.size;
     }
 
+    public Node<T> GetHead()
+    {
+        return this.head;
+    }
+
     public LinkList() {
         head = new Node<T>(null, head);
+        currentNode = head;
         size = 0;
     }
 
@@ -31,13 +36,35 @@ public class LinkList<T>
         if (value == null) {
             throw new NullPointerException("Cannot add null element to the list");
         }        
-        head.next = new Node<T>(value, head);
+        currentNode.next = new Node<T>(value, head);
+        currentNode = currentNode.next ;
         size++;
     }
 
-    // public static void main(String [] args)
-    // {
 
-    // }
+    public T remove(int pos) {
+        try {
+            if (pos > size || pos < 0) {
+                //catching errors
+                throw new IndexOutOfBoundsException("position cannot be greater than size or negative");
+            }
+            
+            Node<T> before = head;
+            for (int i = 1; i <= pos; i++) {
+                before = before.next;
+            }
+            Node<T> destroy = before.next;
+            T saved = destroy.value;
+            before.next = before.next.next;
+            destroy = null;
+            size--;
+            return saved;
+        } catch (Exception e) {
+            throw new IndexOutOfBoundsException(e.toString());
+        }
+        
+
+    }
 }
+
 
